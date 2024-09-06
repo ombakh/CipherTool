891d4782882252f04576c8a9267c57f6868470c4
ombakhshi <ombakh28@gmail.com>
2024-09-06 19:06:07 -0400

    Working on math

---
+++ src/Vigenere.java
@@ -0,0 +1,17 @@
+public class Vigenere {
+
+
+//    public static String encrypt(String text, String key) {
+//        StringBuilder output = new StringBuilder();
+//        text = text.toUpperCase();
+//        key = key.toUpperCase();
+//        int keyIndex = 0;
+//
+//
+//    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
