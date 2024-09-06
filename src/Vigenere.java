e3ccb78802ec696a6eff8aad88b1e96469be37e5
ombakhshi <ombakh28@gmail.com>
2024-09-06 19:18:27 -0400

    theoretical encrypt function (needs comments)

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -1,18 +1,31 @@
 public class Vigenere {
 
 
-//    public static String encrypt(String text, String key) {
-//        StringBuilder output = new StringBuilder();
-//        text = text.toUpperCase();
-//        key = key.toUpperCase();
-//        int keyIndex = 0;
-//
-//
-//    }
+    public static String encrypt(String text, String key) {
+        StringBuilder output = new StringBuilder();
+        text = text.toUpperCase();
+        key = key.toUpperCase();
+        int keyIndex = 0;
+        int keyLength = key.length();
+
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int shift = key.charAt(keyIndex % keyLength) - 'A';
+                char encryptedChar = (char) (((c - 'A' + shift) % 26) + 'A');
+                output.append(encryptedChar);
+                keyIndex++;
+
+            }
+            else {
+                output.append(c);
+            }
+        }
+        return output.toString();
+    }
 
     public static void main(String[] args) {
-        String plaintext = "";
-        String key = "";
+        String plaintext = "hello";
+        String key = "hi";
 
         System.out.println("PLAINTEXT: ");
         System.out.println("KEY");
