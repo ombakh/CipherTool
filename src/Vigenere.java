1aefee134d399e9b93f88f605be348729355fd89
ombakhshi <ombakh28@gmail.com>
2024-09-09 09:26:00 -0400

    Vigenere Cipher functional but kinda ugly

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -2,9 +2,10 @@ public class Vigenere {
 
 
     public static String encrypt(String text, String key) {
-        StringBuilder output = new StringBuilder();
+        StringBuilder result = new StringBuilder();
         text = text.toUpperCase();
         key = key.toUpperCase();
+
         int keyIndex = 0;
         int keyLength = key.length();
 
@@ -12,24 +13,46 @@ public class Vigenere {
             if (Character.isLetter(c)) {
                 int shift = key.charAt(keyIndex % keyLength) - 'A';
                 char encryptedChar = (char) (((c - 'A' + shift) % 26) + 'A');
-                output.append(encryptedChar);
+                result.append(encryptedChar);
                 keyIndex++;
-
-            }
-            else {
-                output.append(c);
+            } else {
+                result.append(c);
             }
         }
-        return output.toString();
+        return result.toString();
+    }
+    public static String decrypt(String plaintext, String key) {
+        StringBuilder result = new StringBuilder();
+        plaintext = plaintext.toUpperCase();
+        key = key.toUpperCase();
+
+        int keyIndex = 0;
+        int keyLength = key.length();
+
+        for (char c : plaintext.toCharArray()) {
+            if (Character.isLetter(c)) {
+                int shift = key.charAt(keyIndex % keyLength) - 'A';
+                char decryptedChar = (char) (((c - 'A' - shift + 26) % 26) + 'A');
+                result.append(decryptedChar);
+                keyIndex++;
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
         String plaintext = "hello";
         String key = "hi";
 
-        System.out.println("PLAINTEXT: ");
-        System.out.println("KEY");
-        System.out.println("ENCRYPTED");
+        String encrypted = encrypt(plaintext, key);
+        String decrypted = decrypt(encrypted, key);
+
+        System.out.println("PLAINTEXT: " + plaintext);
+        System.out.println("KEY: " + key);
+        System.out.println("ENCRYPTED: " + encrypted);
+        System.out.println("DECRYPTED: " + decrypted);
     }
 
 }
