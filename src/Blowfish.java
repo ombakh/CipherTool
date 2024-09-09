06f0b69bd2c8f58c57f0fb1d0c6adbbfcfed3eac
ombakhshi <ombakh28@gmail.com>
2024-09-09 18:54:42 -0400

    encrpytion method theoretically complete

--- src/Blowfish.java
+++ src/Blowfish.java
@@ -15,6 +15,12 @@ public class Blowfish {
 
     public static String encrypt(String plaintext, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.ENCRYPT_MODE, key); // This sets the Cipher to encrypt mode
+
+        //plaintext -> Base64 String
+        byte[] encryptedBites = cipher.doFinal(plaintext.getBytes("UTF-8")); // -> defines text standard
+        return Base64.getEncoder().encodeToString(encryptedBites); // -> puts encrypted bites into string
+
     }
     public static void main(String[] args) throws Exception {
         System.out.println(generateKey());
