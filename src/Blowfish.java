59b93dbf95e027e225c25f0062027b9382de4aab
ombakhshi <ombakh28@gmail.com>
2024-09-09 18:48:15 -0400

    Working on encryption string + new branch

--- src/Blowfish.java
+++ src/Blowfish.java
@@ -6,10 +6,15 @@ import java.util.Base64;
 
 public class Blowfish {
 
+    // generateKey generates a Blowfish key; length based on input value of keyGen.init(#)
     public static SecretKey generateKey() throws Exception {
         KeyGenerator keyGen = KeyGenerator.getInstance("Blowfish");
         keyGen.init(64); // This number is how long the generated key will be
         return keyGen.generateKey();
+    }
+
+    public static String encrypt(String plaintext, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("Blowfish");
     }
     public static void main(String[] args) throws Exception {
         System.out.println(generateKey());
